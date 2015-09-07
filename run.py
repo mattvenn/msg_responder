08f32275357fd1db6b685d300e845319876db38a
matt venn <matt@mattvenn.net>
2015-09-07 11:18:14 +0100

    responds with body and only from my num

--- run.py
+++ run.py
@@ -1,14 +1,30 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+import logging
+from secrets import my_num
  
+# setup logger
+log = logging.getLogger('')
+log.setLevel(logging.DEBUG)
+
+# create console handler and set level to info
+log_format = logging.Formatter('%(asctime)s - %(levelname)-8s - %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(log_format)
+log.addHandler(ch)
+
 app = Flask(__name__)
  
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
- 
+def respond():
+    from_number = request.values.get('From', None) 
+    body = request.values.get('Body', None) 
+    if from_number != my_num:
+        log.info("not responding to invalid number %s" % from_number)
+        return ''
+
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message(body)
     return str(resp)
  
 if __name__ == "__main__":
